ca18c9eec087c46c6001bcd32694ac0a247c1f00
Ivo Jimenez <ivo.jimenez@gmail.com>
2019-05-26 13:03:39 -0700

    Fixes to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -ex
 
-CMAKE_PROJECT_DIR="$PWD"
+if [ -z "$CMAKE_PROJECT_DIR" ] && [ -z "$CMAKE_GIT_REPO" ]; then
+  echo "Expecting one of CMAKE_PROJECT_DIR or CMAKE_GIT_REPO"
+  exit 1
+fi
 
 if [ -n "$CMAKE_GIT_REPO" ]; then
-  CMAKE_PROJECT_DIR="$CMAKE_PROJECT_DIR/$(basename $CMAKE_GIT_REPO)"
+  CMAKE_PROJECT_DIR="$PWD/$(basename $CMAKE_GIT_REPO)"
 
   if [ ! -f "$CMAKE_PROJECT_DIR/.git" ]; then
-    git clone --branch $CMAKE_GIT_REF --depth=1 "$CMAKE_GIT_REPO"
+    if [ -n "$CMAKE_GIT_REF" ]; then
+      git clone --branch "$CMAKE_GIT_REF" --depth=1 "$CMAKE_GIT_REPO"
+    else
+      git clone --depth=1 "$CMAKE_GIT_REPO"
+    fi
   fi
 fi
 
@@ -15,7 +22,7 @@ if [ -n "$CMAKE_INSTALL_DEPS_SCRIPT" ]; then
   $CMAKE_INSTALL_DEPS_SCRIPT
 fi
 
-if [ "$CMAKE_CLEAN" == "true" ] || [ ! -d "$CMAKE_PROJECT_DIR/build"]; then
+if [ "$CMAKE_CLEAN" == "true" ] || [ ! -d "$CMAKE_PROJECT_DIR/build" ]; then
   rm -rf "$CMAKE_PROJECT_DIR/build"
   git -C "$CMAKE_PROJECT_DIR" submodule update --init --recursive
 fi
